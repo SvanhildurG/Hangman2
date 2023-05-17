1c325947b1d73e88db3f3152364634717f90835a
SvanhildurG <114434323+SvanhildurG@users.noreply.github.com>
2023-05-17 10:30:43 +0000

    5 commit

--- lib/main.dart
+++ lib/main.dart
@@ -17,21 +17,21 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.pink,
       ),
-      home: const MyHomePage(title: 'Ahorcado'),
+      home: const MyHangMan(title: 'Ahorcado'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title}); //Homepage
+class MyHangMan extends StatefulWidget {
+  const MyHangMan({super.key, required this.title}); //Homepage
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyHangMan> createState() => _MyHangManState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHangManState extends State<MyHangMan> {
   List<String> words = [
     'lluvia',
     'viento',
@@ -43,32 +43,39 @@ class _MyHomePageState extends State<MyHomePage> {
     'sol',
     'nube'
   ];
+//From tic tac toe - trying to reset game with elevated button
+  //Error: Classes can't be declared inside other classes!!!! - look at next!!!! - Above class _MyHangmanState?
+  class _GameScreen extends State<MyHangMan> {
+  Game game = Game();
+
+  }
+
   String randomWord = '';
   //List<String> _guessedLetters = []; //Already defined
   int _wrongGuesses = 0; //Not using
-  List<String> letterInput = [];
+  //List<String> letterInput = []; //Ath!!!
   List<String> underscore = [];
-  List<String> images = [];
-  final _textController = TextEditingController();
+  List<String> images = [
+    '1.png',
+    '2.png',
+    '3.png',
+    '4.png',
+    '5.png',
+    '6.png',
+    '7.png'
+  ];
 
+  final _textController = TextEditingController(); //Takes input from keyboard
+
+  //Increase/show images one by one if user guesses wrong letter
   void wrongGuesses() {
-    //Try to use counter with images, have set state (Gunnar) and counter but haven't got it to work
-    //void because setState - from error message
     setState(() {
       _wrongGuesses++;
     });
   }
 
-  @override
-  //Call initState method to initialize randomWord and underscore
-  //Within void - don't return value
-  void initState() {
-    super.initState();
-    randomWord = words.elementAt(Random().nextInt(words.length));
-    underscore = List.generate(randomWord.length, (_) => '_');
-  }
-
-  void _guessedLetters(String letter) {
+//Check if letter is to be found in word or not
+  void checkLetter(String letter) {
     bool letterFound = false;
     setState(() {
       for (var i = 0; i < randomWord.length; i++) {
@@ -80,7 +87,17 @@ class _MyHomePageState extends State<MyHomePage> {
       if (!letterFound) {
         wrongGuesses();
       }
+      _textController.clear();
     });
+  }
+
+  @override
+  //Call initState method to initialize randomWord and underscore
+  //Within void - don't return value
+  void initState() {
+    super.initState();
+    randomWord = words.elementAt(Random().nextInt(words.length));
+    underscore = List.generate(randomWord.length, (_) => '_');
   }
 
   Widget build(BuildContext context) {
@@ -96,11 +113,12 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Center(
-              child: Stack(
-                //Trying Stack
-                children: [],
-              ),
+            const SizedBox(height: 16),
+            //Display of images after each wrong guess, using map
+            Image.asset(
+              'assets/images/${images[_wrongGuesses]}', //Works but get error message after 7 images - look at next
+              height: 200,
+              width: 200,
             ),
             const SizedBox(height: 16),
             Text(
@@ -110,20 +128,14 @@ class _MyHomePageState extends State<MyHomePage> {
                 fontWeight: FontWeight.bold,
               ),
             ),
-            const SizedBox(height: 20),
-            Image.asset(
-              //Try later!!!
-              'assets/images/1.png',
-              //'assets/image$_wrongGuesses.png', //Use setState - must be above the widget tree
-              height: 200,
-              width: 200,
-            ),
-            const SizedBox(height: 60),
+            const SizedBox(height: 30),
+            //User can input a letter by tapping on instructions, introducing the letter in the box and press enter
             TextField(
               controller: _textController,
+              onSubmitted: checkLetter,
               decoration: InputDecoration(
                 hintText: '¡Entra tu letra!',
-                hintStyle: TextStyle(
+                hintStyle: const TextStyle(
                   fontWeight: FontWeight.bold,
                   fontStyle: FontStyle.italic,
                   color: Colors.pinkAccent,
@@ -137,14 +149,35 @@ class _MyHomePageState extends State<MyHomePage> {
                   icon: const Icon(Icons.keyboard),
                 ),
               ),
-            )
+            ),
+              Expanded(
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                  child: ElevatedButton.icon(
+                    onPressed: () {
+                 },
+                    icon: Icon(Icons.replay),
+                    label: Text('Jugar de nuevo'),
+                    ),
+                  ),
+                ),
           ],
         ),
       ),
-      //floatingActionButton: FloatingActionButton(
-      //onPressed: (); //Trying keyboard, need set state? and define, maybe comes by itself on Android?: https://www.youtube.com/watch?v=LFQgZc4oKa4
-      //child: const Icon(Icons.keyboard),
-      //), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+//Need a place to keep track of incorrect guessed letters
+//Need a way to stop the game if finished with or without success:
+//give positive feedback on success and jugar de nuevo button
+//show word and end game with jugar de nuevo button if not success
+//Additional requirements:
+//Add homescreen with gif??? BMI calculator section of Flutter Course
+//https://flutterawesome.com/bmi-calculator-using-flutter/:
+/*class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+*/
+//https://github.com/londonappbrewery/BMI-Calculator-Flutter-Completed/blob/master/lib/screens/input_page.dart
